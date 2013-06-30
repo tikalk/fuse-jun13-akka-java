9139114851327f0ef9554b919491cccb96049f34
Noam <metormaon@gmail.com>
2013-06-30 13:47:45 +0300

    Adding infrastructure objects

---
+++ akka-snake-game/src/main/java/infrastructure/Wall.java
@@ -0,0 +1,11 @@
+package infrastructure;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: noam
+ * Date: 6/30/13
+ * Time: 1:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Wall implements BoardElement {
+}
