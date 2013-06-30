e4d7fab7c00b9fd3b3a19845e81cec3cf491bf81
Noam <metormaon@gmail.com>
2013-06-30 14:22:12 +0300

    Revising Player to include Snake

--- akka-snake-game/src/main/java/akka/snake/game/java/Player.java
+++ akka-snake-game/src/main/java/akka/snake/game/java/Player.java
@@ -1,5 +1,23 @@
 package akka.snake.game.java;
 
-public class Player {
+import infrastructure.*;
+import infrastructure.Snake;
 
+public class Player {
+    private boolean isAlive;
+
+    private infrastructure.Snake snake;
+
+    public Player() {
+        isAlive=true;
+    }
+
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
 }
